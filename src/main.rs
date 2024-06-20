7cec31073cf935792a1a1c6f6be368ee3b8fe403
oberrich <oberrich@img.ws>
2024-06-20 22:22:38 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,266 @@
+use core::{
+    fmt,
+    fmt::{Display, Formatter},
+};
+use std::{ffi::OsStr, fs};
+use walkdir::{DirEntry, WalkDir};
+
+enum VersionControlSystem {
+    Git,
+    Svn,
+}
+
+enum DirectoryClassification {
+    Regular,
+    VersionControl(VersionControlSystem),
+}
+
+enum SpreadsheetFileType {
+    Excel,
+    Csv(char),
+}
+
+enum DocumentFileType {
+    Pdf,
+    Text,
+    Word,
+}
+
+enum SecretFileType {
+    Env,
+}
+
+enum ConfigurationFileType {
+    Yaml,
+    Json,
+    Ini,
+}
+
+enum DatabaseFileType {
+    Sqlite,
+    Sql,
+    Db,
+    Pdb,
+}
+
+enum ArchiveFileType {
+    Zip,
+    Rar,
+}
+
+enum FileClassification {
+    Regular,
+    Secret(SecretFileType),
+    Spreadsheet(SpreadsheetFileType),
+    Document(DocumentFileType),
+    Configuration(ConfigurationFileType),
+    Database(DatabaseFileType),
+    Archive(ArchiveFileType),
+}
+
+enum EntryClassification {
+    File(FileClassification),
+    Dir(DirectoryClassification),
+}
+
+const PLATFORM_DIR_SEP: char = '\\';
+const PLATFORM_SYS_DIR: &str = "C:\\Windows";
+
+trait OptionFlatStringExt {
+    fn to_lowercase(&self) -> Option<String>;
+}
+
+impl OptionFlatStringExt for Option<&OsStr> {
+    fn to_lowercase(&self) -> Option<String> {
+        self.map(|x| x.to_string_lossy().to_ascii_lowercase())
+    }
+}
+
+trait DirEntryExt {
+    fn classify(&self) -> EntryClassification;
+    fn classify_dir(&self) -> DirectoryClassification;
+    fn classify_file(&self) -> FileClassification;
+    fn is_allowed(&self) -> bool;
+    fn is_blacklisted(&self) -> bool;
+    fn is_sys_dir(&self) -> bool;
+}
+
+impl DirEntryExt for DirEntry {
+    fn is_sys_dir(&self) -> bool {
+        self.path()
+            .file_name()
+            .and_then(OsStr::to_str)
+            .map(|s| s == PLATFORM_SYS_DIR)
+            .unwrap_or(false)
+    }
+
+    fn is_blacklisted(&self) -> bool {
+        self.is_sys_dir()
+    }
+
+    fn is_allowed(&self) -> bool {
+        !self.is_blacklisted()
+    }
+
+    fn classify_file(&self) -> FileClassification {
+        let path = self.path();
+        let file_name = path.file_name();
+        let extension = path.extension();
+        match file_name.to_lowercase().as_deref() {
+            Some(".env") => FileClassification::Secret(SecretFileType::Env),
+            Some(_) => match extension.to_lowercase().as_deref() {
+                Some("xlsl") => FileClassification::Spreadsheet(SpreadsheetFileType::Excel),
+                Some("csv") => {
+                    let mut seps = [
+                        (char::default(), 1usize),
+                        (',', 0),
+                        ('\t', 0),
+                        (':', 0),
+                        (';', 0),
+                        ('|', 0),
+                        (' ', 0),
+                    ];
+
+                    let csv_chars = fs::read_to_string(self.path()).unwrap_or_default();
+                    for (sep, count) in &mut seps {
+                        *count += csv_chars
+                            .chars()
+                            .take(50_000)
+                            .filter(|&c| c == *sep)
+                            .count();
+                    }
+                    seps.sort_by(|a, b| b.1.cmp(&a.1));
+                    FileClassification::Spreadsheet(SpreadsheetFileType::Csv(seps[0].0))
+                }
+                Some("txt") | Some("log") => FileClassification::Document(DocumentFileType::Text),
+                Some("pdf") => FileClassification::Document(DocumentFileType::Pdf),
+                Some(
+                    "rtf" | "xlsx" | "odt" | "xps" | "wps" | "dotx" | "dotm" | "docx" | "docm"
+                    | "doc",
+                ) => FileClassification::Document(DocumentFileType::Word),
+                Some("db") | Some("dump") => FileClassification::Database(DatabaseFileType::Db),
+                Some("sqlite") | Some("sqlite3") => {
+                    FileClassification::Database(DatabaseFileType::Sqlite)
+                }
+                Some("sql") | Some("mysql") | Some("pgsql") => {
+                    FileClassification::Database(DatabaseFileType::Sql)
+                }
+                Some("pdb") => FileClassification::Database(DatabaseFileType::Pdb),
+                Some("yaml") => FileClassification::Configuration(ConfigurationFileType::Yaml),
+                Some("json") => FileClassification::Configuration(ConfigurationFileType::Json),
+                Some("ini") => FileClassification::Configuration(ConfigurationFileType::Ini),
+                Some("zip") => FileClassification::Archive(ArchiveFileType::Zip),
+                Some("rar") => FileClassification::Archive(ArchiveFileType::Rar),
+                _ => FileClassification::Regular,
+            },
+            None => FileClassification::Regular,
+        }
+    }
+
+    fn classify_dir(&self) -> DirectoryClassification {
+        let path = self.path();
+        let file_name = path.file_name();
+        match file_name.to_lowercase().as_deref() {
+            Some(".git") => DirectoryClassification::VersionControl(VersionControlSystem::Git),
+            Some(".svn") => DirectoryClassification::VersionControl(VersionControlSystem::Svn),
+            _ => DirectoryClassification::Regular,
+        }
+    }
+
+    fn classify(&self) -> EntryClassification {
+        if self.file_type().is_dir() {
+            EntryClassification::Dir(self.classify_dir())
+        } else {
+            EntryClassification::File(self.classify_file())
+        }
+    }
+}
+
+impl Display for DirectoryClassification {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            DirectoryClassification::VersionControl(vcs) => match vcs {
+                VersionControlSystem::Git => write!(f, "git"),
+                VersionControlSystem::Svn => write!(f, "svn"),
+            },
+            DirectoryClassification::Regular => Ok(()),
+        }
+    }
+}
+
+impl Display for FileClassification {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Secret(ext) => match ext {
+                SecretFileType::Env => write!(f, "dotenv"),
+            },
+            Self::Spreadsheet(ext) => match ext {
+                SpreadsheetFileType::Excel => write!(f, "excel"),
+                SpreadsheetFileType::Csv(separator) => write!(f, "csv('{}')", separator),
+            },
+            Self::Document(ext) => match ext {
+                DocumentFileType::Pdf => write!(f, "pdf"),
+                DocumentFileType::Text => write!(f, "txt"),
+                DocumentFileType::Word => write!(f, "word"),
+            },
+            Self::Database(ext) => match ext {
+                DatabaseFileType::Sqlite => write!(f, "sqlite"),
+                DatabaseFileType::Sql => write!(f, "sql"),
+                DatabaseFileType::Db => write!(f, "db"),
+                DatabaseFileType::Pdb => write!(f, "pdb"),
+            },
+            Self::Configuration(ext) => match ext {
+                ConfigurationFileType::Yaml => write!(f, "yaml"),
+                ConfigurationFileType::Json => write!(f, "json"),
+                ConfigurationFileType::Ini => write!(f, "ini"),
+            },
+            Self::Archive(ext) => match ext {
+                ArchiveFileType::Zip => write!(f, "zip"),
+                ArchiveFileType::Rar => write!(f, "rar"),
+            },
+            Self::Regular => Ok(()),
+        }
+    }
+}
+
+impl Display for EntryClassification {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::File(class) => write!(f, "{}", class),
+            Self::Dir(class) => write!(f, "{}", class),
+        }
+    }
+}
+
+fn scan_drive(letter: char) -> anyhow::Result<()> {
+    for entry in WalkDir::new(format!("{}:{}", letter, PLATFORM_DIR_SEP))
+        .follow_links(true)
+        .into_iter()
+        .filter_entry(|e| e.is_allowed())
+        .filter_map(|e: std::result::Result<DirEntry, walkdir::Error>| e.ok())
+    {
+        let classification = entry.classify();
+        match &classification {
+            EntryClassification::File(class) => match class {
+                FileClassification::Regular
+                | FileClassification::Document(DocumentFileType::Text)
+                | FileClassification::Spreadsheet(SpreadsheetFileType::Csv('\0')) => continue,
+                _ => {}
+            },
+            EntryClassification::Dir(DirectoryClassification::Regular) => continue,
+            _ => {}
+        }
+
+        println!("{} # {}", entry.path().display(), classification);
+    }
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    for letter in 'A'..='Z' {
+        scan_drive(letter)?;
+    }
+
+    Ok(())
+}
